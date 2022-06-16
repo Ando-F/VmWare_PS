e343f7a02385f063dceaeec11a1577f8a1ab7d1c
Ando-F <87995797+Ando-F@users.noreply.github.com>
2022-06-16 16:50:36 +0300

    Update HA_restart_event.ps1

--- HA_restart_event.ps1
+++ HA_restart_event.ps1
@@ -1,1 +1,7 @@
+Получение событий по HA перезапуску ВМ (vSphere HA restarted virtual machine)
 
+
+Get-VMHost -Location CLUSTER-*** | Get-VM | Get-VIEvent |
+  where {$_.FullFormattedMessage -match "vSphere HA restarted virtual machine"} |
+  select ObjectName,@{N="IP addr";E={(Get-view -Id $_.Vm.Vm).Guest.IpAddress}},CreatedTime,FullFormattedMessage |
+  Export-Csv -Path 'D:\tmp\exportCSV\HA-***.csv' -Delimiter ';' -Encoding UTF8
